86dbbe1f258239377b3442542fe041afcfd3e785
Ibrahim <ibrahim.hassan2402@gmail.com>
2024-09-01 23:54:53 +0100

    Bot can go live when program is run.

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 import discord
 from discord.ext import commands
 import os
-import dice
 
 # Load environment variables
 from dotenv import load_dotenv
 load_dotenv()
 
-client = commands.Bot(command_prefix='/')
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(command_prefix='!', intents=intents)
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
 
-@client.command()
-async def roll(ctx:discord.Interaction, dice_str:str):
-    result = dice.roll(dice_str)
-    await ctx.send(result)
-
 try:
     client.run(os.getenv('BOT_TOKEN'))
 except Exception as e:
-    print(e)
+    print("Couldn't resolve token", e)
