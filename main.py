d72208f839d0daf192115b4f8fc3a5b22f370728
Ibrahim <ibrahim.hassan2402@gmail.com>
2024-09-04 03:29:38 +0100

    Roll functionality working.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
+from discord import app_commands
 from discord.ext import commands
 import os
+import dice
 
 # Load environment variables
 from dotenv import load_dotenv
@@ -11,10 +13,27 @@ intents.message_content = True
 
 client = commands.Bot(command_prefix='!', intents=intents)
 
+# When the bot is initialised, sync command tree
 @client.event
 async def on_ready():
     print('Bot is ready.')
+    try:
+        await client.tree.sync()
+    except Exception as e:
+        print("Couldn't sync commands", e)
 
+# Slash commands
+@client.tree.command(name="roll", description="Roll a dice in NdN format")
+async def roll(interaction:discord.Interaction, dice_input:str):
+    try:
+        result:list = dice.roll(dice_input)
+        await interaction.response.send_message("You rolled: " + str(result))
+        return
+    except Exception as e:
+        await interaction.response.send_message("Format has to be in NdN!", ephemeral=True)
+        return
+
+# Running the bot, keep at the end of the file
 try:
     client.run(os.getenv('BOT_TOKEN'))
 except Exception as e:
